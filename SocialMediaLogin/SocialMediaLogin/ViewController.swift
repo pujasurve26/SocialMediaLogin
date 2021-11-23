94cc24b04e6a8dba5ef313eb695d5416922be48d
Puja Kalpesh Surve <pujakalpesh@Pujas-MacBook-Air.local>
2021-11-23 10:57:20 +0530

    Initial commit

---
+++ SocialMediaLogin/SocialMediaLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMediaLogin
+//
+//  Created by Puja Kalpesh Surve on 23/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
